761e97a219190a3c9dcb4e1851b66e94293a93ad
giannifontanot <giannifontanot@gmail.com>
2021-08-07 16:50:53 -0500

    Minimum server requirements. No routes yet.

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const {sequelize, Students} = require('./models/index');
+
+
+const app = express();
+app.use(express.json());
+
+app.listen({port: 3000,},async ()=>{
+
+    //lets check the connection with MySQL
+    await sequelize.authenticate();
+    console.log('authenticated to MySQL');
+    await sequelize.sync({force: true});
+    console.log('Database sync complete');
+});
+
+
+
