95e3b6dbfa0427d300dc65414b5c44ef5d5950cd
giannifontanot <giannifontanot@gmail.com>
2021-08-08 17:47:40 -0500

    Connection established, the app reads and insert students in the database

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 const express = require('express');
-
 const {sequelize, Student} = require('./models/index');
+const PORT = process.env.PORT || 3001;
 
 
 const app = express();
 app.use(express.json());
 
-app.listen({port: 3000,},async ()=>{
+app.post('/student', async (req, res) => {
+    try {
+        const {id, firstName, lastName} = req.body;
+        const student = await Student.create({id, firstName, lastName});
+        console.log("student: " + JSON.stringify(student));
+
+        res.status(202).json(student);
+
+    } catch (err) {
+        res.status(500).send(">>>>>" + err.message);
+    }
+});
+
+app.get('/allStudents', (req, res) => {
+    try {
+        const students = Student.findAll();
+        return res.status(200).json(students);
+    } catch (e) {
+        return res.status(500).send(e.message);
+    }
+})
+
+app.listen({port: 3000}, async () => {
 
     //lets check the connection with MySQL
-    await sequelize.authenticate();
+    // await sequelize.authenticate();
     console.log('authenticated to MySQL');
+
     await sequelize.sync({force: true});
     console.log('Database sync complete');
 });
--- models/student.js
+++ models/student.js
@@ -1,25 +1,24 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const {Model} = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
-  class Student extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Student.init({
-    id: {type: DataTypes.INTEGER, primaryKey: true},
-    firstName: {type: DataTypes.STRING, allowNull: false},
-    lastName: {type: DataTypes.STRING, allowNull: false}
-  }, {
-    sequelize,
-    modelName: 'Student',
-  });
-  return Student;
+    class Student extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    };
+    Student.init({
+        id: {type: DataTypes.INTEGER, primaryKey: true},
+        firstName: {type: DataTypes.STRING, allowNull: false},
+        lastName: {type: DataTypes.STRING, allowNull: false}
+    }, {
+        sequelize,
+        modelName: 'Student',
+    });
+    return Student;
 };
