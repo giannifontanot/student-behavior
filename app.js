66d4bca23c9974985addd78817f27194d063f88d
giannifontanot <giannifontanot@gmail.com>
2021-08-13 14:21:39 -0500

    Changes to try to upload to Heroku.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const sequelize = require('./config/connection');
-const peasant = require('./seed/index');
-const path = require('path');
+//const sequelize = require('./config/connection');
+//const peasant = require('./seed/index');
+//      const path = require('path');
 const router = require('./routes/index');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,14 +10,14 @@ app.use(express.json());
 app.use(router);
 
 
-sequelize.sync({force: false}).then(
+//sequelize.sync({force: false}).then(
     app.listen(PORT, () => {
-        peasant.sow();
+        //peasant.sow();
         console.log('Sever running on port: %j', PORT);
         console.log('http://localhost:%j/', PORT);
         console.log('http://localhost:%j/api/', PORT);
         console.log('http://localhost:%j/api/Allstudents', PORT);
         console.log('http://localhost:%j/api/insertStudent', PORT);
     })
-);
+//);
 
