57fa969a92ae42ea406772fe8ccc97a078e0f7b9
giannifontanot <giannifontanot@gmail.com>
2021-08-14 10:07:54 -0500

    Message on screen.

--- routes/myApi/studentRoutes.js
+++ routes/myApi/studentRoutes.js
@@ -4,19 +4,21 @@ const {Student} = require('../../models/index');
 
 router.get('/allStudents', async (req, res) => {
     try {
-        const students = await Student.findAll();
-        console.log("---> students :" + JSON.stringify(students));
-        return res.status(200).json(students);
+        return res.status(200).send('<H1>This is ALL STUDENTS page</H1>'); // --------------------------------------------------------------------------
+        // const students = await Student.findAll();
+        // console.log("---> students :" + JSON.stringify(students));
+        // return res.status(200).json(students);
     } catch (e) {
         return res.status(500).send(e.message);
     }
 });
 
 router.get('/insertStudent', async(req, res) => {
-     console.log("---> insertStudent :" );
-    const  studentInsert  = await Student.create({id:2,firstName:"yo",lastName:"merito"})
-     console.log("---> studentInsert :" + studentInsert );
-        res.status(200).json(studentInsert);
+        return res.status(200).send('<H1>This is INSERT STUDENTS page</H1>'); // --------------------------------------------------------------------------
+    //  console.log("---> insertStudent :" );
+    // const  studentInsert  = await Student.create({id:2,firstName:"yo",lastName:"merito"})
+    //  console.log("---> studentInsert :" + studentInsert );
+    //     res.status(200).json(studentInsert);
 })
 
 router.get('/', (req, res) => {
