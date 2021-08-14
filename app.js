4e97496e76b80414917b562c85844b2a9eb3f7f7
giannifontanot <giannifontanot@gmail.com>
2021-08-14 10:58:55 -0500

    Uncommented code in oder to work in Heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-//const sequelize = require('./config/connection');
-//const peasant = require('./seed/index');
-//      const path = require('path');
+const sequelize = require('./config/connection');
+const peasant = require('./seed/index');
+const path = require('path');
 const router = require('./routes/index');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(router);
 
 
-//sequelize.sync({force: false}).then(
+sequelize.sync({force: false}).then(
     app.listen(PORT, () => {
         //peasant.sow();
         console.log('Sever running on port: %j', PORT);
@@ -19,5 +19,5 @@ app.use(router);
         console.log('http://localhost:%j/api/Allstudents', PORT);
         console.log('http://localhost:%j/api/insertStudent', PORT);
     })
-//);
+);
 
--- routes/myApi/studentRoutes.js
+++ routes/myApi/studentRoutes.js
@@ -1,24 +1,22 @@
 const router = require('express').Router();
-//const {Student} = require('../../models/index');
+const {Student} = require('../../models/index');
 
 
 router.get('/allStudents', async (req, res) => {
     try {
-        return res.status(200).send('<H1>This is ALL STUDENTS page</H1>'); // --------------------------------------------------------------------------
-        // const students = await Student.findAll();
-        // console.log("---> students :" + JSON.stringify(students));
-        // return res.status(200).json(students);
+        const students = await Student.findAll();
+        console.log("---> students :" + JSON.stringify(students));
+        return res.status(200).json(students);
     } catch (e) {
         return res.status(500).send(e.message);
     }
 });
 
-router.get('/insertStudent', async(req, res) => {
-        return res.status(200).send('<H1>This is INSERT STUDENTS page</H1>'); // --------------------------------------------------------------------------
-    //  console.log("---> insertStudent :" );
-    // const  studentInsert  = await Student.create({id:2,firstName:"yo",lastName:"merito"})
-    //  console.log("---> studentInsert :" + studentInsert );
-    //     res.status(200).json(studentInsert);
+router.get('/insertStudent', async (req, res) => {
+    console.log("---> insertStudent :");
+    const studentInsert = await Student.create({id: 2, firstName: "yo", lastName: "merito"})
+    console.log("---> studentInsert :" + studentInsert);
+    res.status(200).json(studentInsert);
 })
 
 router.get('/', (req, res) => {
